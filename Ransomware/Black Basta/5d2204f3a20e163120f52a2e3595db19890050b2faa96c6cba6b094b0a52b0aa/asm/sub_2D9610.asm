fceef4360b6c87dd54f85df9f48db75a521b940e
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-03 13:32:43 -0400

    Add work.

---
+++ Ransomware/Black Basta/5d2204f3a20e163120f52a2e3595db19890050b2faa96c6cba6b094b0a52b0aa/asm/sub_2D9610.asm
@@ -0,0 +1,6 @@
+.text:002D9610 ; void __thiscall sub_2D9610(void *)
+.text:002D9610 sub_2D9610 proc near                    ; CODE XREF: _main+95↓p
+.text:002D9610                                         ; _main+C3↓p ...
+.text:002D9610 jmp     sub_2E2BC0
+.text:002D9610 sub_2D9610 endp
+.text:002D9610
