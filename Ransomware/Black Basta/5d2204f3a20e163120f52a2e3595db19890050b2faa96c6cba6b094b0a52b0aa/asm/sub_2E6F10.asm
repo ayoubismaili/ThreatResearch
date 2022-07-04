9267d9e7b4dc167ce4c02c90564d54e1d41b9da0
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-04 16:17:11 -0400

    Add work.

---
+++ Ransomware/Black Basta/5d2204f3a20e163120f52a2e3595db19890050b2faa96c6cba6b094b0a52b0aa/asm/sub_2E6F10.asm
@@ -0,0 +1,13 @@
+.text:002E6F10 sub_2E6F10 proc near                    ; CODE XREF: _main+101↓p
+.text:002E6F10
+.text:002E6F10 arg_0= dword ptr  8
+.text:002E6F10
+.text:002E6F10 push    ebp
+.text:002E6F11 mov     ebp, esp
+.text:002E6F13 mov     eax, [ebp+arg_0]
+.text:002E6F16 mov     ecx, [ecx+4]
+.text:002E6F19 mov     [eax], ecx
+.text:002E6F1B pop     ebp
+.text:002E6F1C retn    4
+.text:002E6F1C sub_2E6F10 endp
+.text:002E6F1C
