d38c1c91bd46cc8a6232d0495ab8dd9c3bee8c29
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-15 09:04:59 -0400

    Add work.

---
+++ Ransomware/Black Basta/5d2204f3a20e163120f52a2e3595db19890050b2faa96c6cba6b094b0a52b0aa/asm/sub_2F5124.asm
@@ -0,0 +1,7 @@
+.text:002F5124 sub_2F5124 proc near                    ; CODE XREF: sub_2E9970+43↑p
+.text:002F5124 push    ebp
+.text:002F5125 mov     ebp, esp
+.text:002F5127 pop     ebp
+.text:002F5128 jmp     sub_2F507E
+.text:002F5128 sub_2F5124 endp
+.text:002F5128
