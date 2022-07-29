67b7fded834fac8b627e0b770197e102d1db4dde
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-29 16:49:23 -0400

    Add work.

---
+++ Trojan/HunterStealer/e62467fc0e7982279b2752a3330e741135366e7b318890aa9613ccc5ddcfd37f/asm/sub_450504.asm
@@ -0,0 +1,6 @@
+.text:00450504 ; int __thiscall sub_450504(void *Object)
+.text:00450504 sub_450504 proc near                    ; CODE XREF: .text:00450380↑p
+.text:00450504 mov     eax, [ecx]
+.text:00450506 retn
+.text:00450506 sub_450504 endp
+.text:00450506
