edba10aec2c725254298140edb5f4f9beb8eac18
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-29 13:53:50 -0400

    Add work.

---
+++ Trojan/HunterStealer/e62467fc0e7982279b2752a3330e741135366e7b318890aa9613ccc5ddcfd37f/asm/sub_452596.asm
@@ -0,0 +1,6 @@
+.text:00452596 sub_452596 proc near                    ; CODE XREF: sub_443A4F:loc_443BA3↑p
+.text:00452596                                         ; sub_443A4F:loc_443BA8↑p ...
+.text:00452596 push    offset aVectorTooLong           ; "vector too long"
+.text:0045259B call    sub_455548
+.text:0045259B sub_452596 endp
+.text:0045259B
