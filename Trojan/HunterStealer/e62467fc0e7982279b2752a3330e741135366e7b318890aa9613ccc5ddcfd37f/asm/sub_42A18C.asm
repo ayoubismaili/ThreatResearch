310454ebb0858ef472a3145ef2dd393832acc3fa
Ayoub Ismaili <ayoubismaili1@gmail.com>
2022-07-31 19:56:16 -0400

    Add work.

---
+++ Trojan/HunterStealer/e62467fc0e7982279b2752a3330e741135366e7b318890aa9613ccc5ddcfd37f/asm/sub_42A18C.asm
@@ -0,0 +1,42 @@
+.text:0042A18C ; int __thiscall sub_42A18C(void *Object)
+.text:0042A18C sub_42A18C proc near                    ; CODE XREF: sub_42D2EA+32↓p
+.text:0042A18C mov     edx, ecx
+.text:0042A18E push    esi
+.text:0042A18F push    edi
+.text:0042A190 xor     edi, edi
+.text:0042A192 mov     [edx+0F7Ch], edx
+.text:0042A198 lea     eax, [edx+9E8h]
+.text:0042A19E mov     [edx+0F80h], eax
+.text:0042A1A4 lea     ecx, [edx+0E68h]
+.text:0042A1AA mov     dword ptr [edx+0F84h], offset dword_47CF50
+.text:0042A1B4 lea     eax, [edx+8F4h]
+.text:0042A1BA mov     dword ptr [edx+0F88h], 101h
+.text:0042A1C4 mov     dword ptr [edx+0F8Ch], 11Eh
+.text:0042A1CE push    0Fh
+.text:0042A1D0 pop     esi
+.text:0042A1D1 mov     [edx+0F90h], esi
+.text:0042A1D7 mov     [edx+0F94h], edi
+.text:0042A1DD mov     [edx+0F98h], eax
+.text:0042A1E3 lea     eax, [edx+0EE0h]
+.text:0042A1E9 mov     [edx+0F9Ch], ecx
+.text:0042A1EF mov     dword ptr [edx+0FA0h], offset dword_47CFD0
+.text:0042A1F9 mov     [edx+0FA4h], edi
+.text:0042A1FF mov     dword ptr [edx+0FA8h], 1Eh
+.text:0042A209 mov     [edx+0FACh], esi
+.text:0042A20F mov     [edx+0FB0h], edi
+.text:0042A215 mov     [edx+0FB4h], eax
+.text:0042A21B mov     eax, edx
+.text:0042A21D mov     [edx+0FB8h], edi
+.text:0042A223 mov     dword ptr [edx+0FBCh], offset unk_47CF00
+.text:0042A22D mov     [edx+0FC0h], edi
+.text:0042A233 mov     dword ptr [edx+0FC4h], 13h
+.text:0042A23D mov     dword ptr [edx+0FC8h], 7
+.text:0042A247 mov     [edx+0FCCh], edi
+.text:0042A24D mov     [edx+1AF18h], edi
+.text:0042A253 mov     [edx+1AF1Ch], edi
+.text:0042A259 mov     [edx+1AF20h], edi
+.text:0042A25F pop     edi
+.text:0042A260 pop     esi
+.text:0042A261 retn
+.text:0042A261 sub_42A18C endp
+.text:0042A261
---
+++ Trojan/HunterStealer/e62467fc0e7982279b2752a3330e741135366e7b318890aa9613ccc5ddcfd37f/md/sub_42A18C.md
@@ -0,0 +1,69 @@
+# sub_42A18C function
+
+## Tasks
+
+- [ ] Add Description.
+- [X] Add Syntax.
+- [X] Add Assembly.
+- [ ] Add Source.
+- [ ] Add Arguments.
+- [ ] Add Return Value.
+- [X] Add Dependencies.
+- [X] Add Used By.
+- [X] Add Graph.
+- [ ] Add Flow.
+- [ ] Add Pseudo-code.
+- [ ] Fully documented (Including dependencies).
+
+## Description
+
+(Add description.)
+
+## Syntax
+
+```c
+int __thiscall sub_42A18C(void *Object)
+{...}
+```
+
+## Assembly
+
+Go to [assembly](../asm/sub_42A18C.asm).
+
+## Source
+
+Go to [source](../cc/sub_42A18C.cc).
+
+## Arguments
+
+(Add arguments.)
+
+## Return Value
+
+(Add return value.)
+
+## Dependencies
+
+* Function dependencies:
+  * ...
+
+
+* Data dependencies:
+  * ...
+
+## Used By
+
+* Used by functions:
+  * [`sub_42D2EA`](../md/sub_42D2EA.md)
+
+## Graph
+
+![sub_42A18C Graph](../svg/sub_42A18C.svg "sub_42A18C Graph")
+
+## Flow
+
+(Add flow.)
+
+## Pseudo-code
+
+(Add pseudo-code.)
